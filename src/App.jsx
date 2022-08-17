f7ff5dcda93b7899a7ce1f0c615b77d6cd9c6588
Andre Coelho <andre.junior@adp.com>
2022-08-17 14:59:27 -0300

    aula sobre propriedades

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import { Post } from "./Post";
 export function App() {
   return (
     <div>
-      <Post />
-      <Post />
-      <Post />
-      <Post />
-      <Post />
+      <Post
+        author="André Coelho"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium fugiat autem illo reprehenderit in provident voluptates ipsum culpa asperiores harum sapiente officia quisquam quae ea, assumenda natus libero unde? Dicta!"
+      />
+      <Post author="Ana Carolina" content="Linda demais!! <3" />
+      <Post author="Bernardo" content="É uma criança!! <3" />
     </div>
   );
 }
