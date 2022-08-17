ecd81578e4065044e2c76c2115300f65afa1b179
Andre Coelho <andre.junior@adp.com>
2022-08-17 15:48:00 -0300

    aula sobre CSS Modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+import { Header } from "./components/Header";
 import { Post } from "./Post";
+
+import "./style.css";
 
 export function App() {
   return (
     <div>
+      <Header />
       <Post
         author="André Coelho"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium fugiat autem illo reprehenderit in provident voluptates ipsum culpa asperiores harum sapiente officia quisquam quae ea, assumenda natus libero unde? Dicta!"
