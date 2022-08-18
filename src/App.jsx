4198b31991e8d642253e58223cec13c8a7fdd1bb
Andre Coelho <andre.junior@adp.com>
2022-08-18 10:16:10 -0300

    Estrutura da aplicação - Componente: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,29 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
 
+import styles from "./App.module.css";
+
 import "./global.css";
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="André Coelho"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium fugiat autem illo reprehenderit in provident voluptates ipsum culpa asperiores harum sapiente officia quisquam quae ea, assumenda natus libero unde? Dicta!"
-      />
-      <Post author="Ana Carolina" content="Linda demais!! <3" />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="André Coelho"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium fugiat autem illo reprehenderit in provident voluptates ipsum culpa asperiores harum sapiente officia quisquam quae ea, assumenda natus libero unde? Dicta!"
+          />
+          <Post
+            author="Ana Carolina"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aspernatur eligendi corrupti ratione nostrum corporis nisi quia totam laudantium, maxime aperiam doloribus perferendis possimus cum illum quas cumque voluptatem eos deserunt."
+          />
+        </main>
+      </div>
     </div>
   );
 }
