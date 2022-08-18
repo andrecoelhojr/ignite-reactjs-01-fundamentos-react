2d445a1400501be1a6f9cf47ca88b383c55352b3
Andre Coelho <andre.junior@adp.com>
2022-08-18 14:06:06 -0300

    Estrutura da aplicação - Estilizações do Comentário

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -20,7 +20,7 @@ export function Comment() {
             </div>
 
             <button title="Deletar comentário">
-              <Trash size={20} />
+              <Trash size={24} />
             </button>
           </header>
           <p> Muito bom Andŕe, parabéns!!</p>
