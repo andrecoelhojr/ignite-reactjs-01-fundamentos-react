d6fdcf6bb33e708452d1f0f03efc994aec0eb514
Andre Coelho <andre.junior@adp.com>
2022-08-18 13:43:59 -0300

    Estrutura da aplicação - Componente: Comment

---
+++ src/components/Comment.jsx
@@ -0,0 +1,38 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/andrecoelhojr.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>André Coelho</strong>
+              <time
+                title="18 de Agosto às 10:55"
+                dateTime="2022-08-18 10:55:30"
+              >
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={20} />
+            </button>
+          </header>
+          <p> Muito bom Andŕe, parabéns!!</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
